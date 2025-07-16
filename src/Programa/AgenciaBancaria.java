924f6d4c3d28afa08d8a4a9b4cbe0d05d3ff9638
Kenzo Ribeiro Toda <kenzoribeirotoda@gmail.com>
2025-07-16 08:49:17 -0300

    Criando a classe da Agência

---
+++ src/Programa/AgenciaBancaria.java
@@ -0,0 +1,108 @@
+package Programa;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AgenciaBancaria {
+
+    private Scanner input = new Scanner(System.in);
+    private ArrayList<Conta> contasBancarias = new ArrayList<>();
+
+    public static void main(String[] args) {
+        AgenciaBancaria agencia = new AgenciaBancaria();
+        agencia.operacoes();
+    }
+
+    public void operacoes() {
+
+        System.out.println("------------------------------------------------------");
+        System.out.println("--------------Bem vindos a nossa Agência--------------");
+        System.out.println("------------------------------------------------------");
+        System.out.println("***** Selecione uma operação que deseja realizar *****");
+        System.out.println("------------------------------------------------------");
+        System.out.println("|   Opção 1 - Criar conta   |");
+        System.out.println("|   Opção 2 - Depositar     |");
+        System.out.println("|   Opção 3 - Sacar         |");
+        System.out.println("|   Opção 4 - Transferir    |");
+        System.out.println("|   Opção 5 - Listar        |");
+        System.out.println("|   Opção 6 - Sair          |");
+
+        int operacao = input.nextInt();
+
+        switch (operacao) {
+            case 1:
+                criarConta();
+                break;
+            case 2:
+                depositar();
+                break;
+            case 3:
+                sacar();
+                break;
+            case 4:
+                transferir();
+                break;
+            case 5:
+                listar();
+                break;
+            case 6:
+                System.out.println("Até mais, Obrigado!");
+                System.exit(0);
+            default:
+                System.out.println("Opção inválida!");
+                operacoes();
+                break;
+        }
+    }
+
+    public void criarConta() {
+
+        System.out.println("\nNome: ");
+        String nome = input.next();
+
+        System.out.println("\nCPF: ");
+        String cpf = input.next();;
+
+        System.out.println("\nEmail: ");
+        String email = input.next();
+
+        Pessoa pessoa = new Pessoa(nome, email, cpf);
+
+        Conta conta = new Conta(pessoa);
+
+        contasBancarias.add(conta);
+        System.out.println("Sua conta foi criada com sucesso!");
+
+        operacoes();
+    }
+
+    private Conta encontrarConta(int numeroConta) {
+        Conta conta = null;
+        if (contasBancarias.size() > 0) {
+            for (Conta c : contasBancarias) {
+                if (c.getNumeroConta() == numeroConta) {
+                    conta = c;
+                }
+            }
+        }
+        return conta;
+    }
+
+    public void depositar() {
+        System.out.println("Número da conta: ");
+        int numeroConta = input.nextInt();
+
+        Conta conta = encontrarConta(numeroConta);
+
+        if (conta != null) {
+            System.out.println("Qual valor você deseja depositar?");
+            Double valorDeposito = input.nextDouble();
+            conta.depositar(valorDeposito);
+        } else {
+            System.out.println("Conta não encontrada!");
+        }
+    }
+
+    
+
+}
